235afbebdc62a80299cfdec703aac68e598f19da
Dmiranda <Dmiranda@LAPDMIRANDA.consulting.local>
2020-03-23 22:27:01 -0600

    Subimos el esqueleto del proyecto

---
+++  Spring-boot-jsf-mexa/src/main/java/com/milmonosmilmaquinas/bootprimefaces/SpringBootJsfMexaApplication.java
@@ -0,0 +1,13 @@
+package com.milmonosmilmaquinas.bootprimefaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJsfMexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJsfMexaApplication.class, args);
+	}
+
+}
---
+++  Spring-boot-jsf-mexa/src/test/java/com/milmonosmilmaquinas/bootprimefaces/SpringBootJsfMexaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milmonosmilmaquinas.bootprimefaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJsfMexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
